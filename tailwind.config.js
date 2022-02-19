c9162b9dabb21947b7595de667afca59476c0fea
Isaac Mattern <isaac.mattern@gmail.com>
2022-02-19 13:29:01 -0500

    update bio, change resume link to button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  purge: [
-    './*.html',
-  ],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./*.html'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
